801d91c046df9c0087c252294ba98abdbb29e528
LollipopGeneral <45588935+LollipopGeneral@users.noreply.github.com>
2019-01-04 00:11:39 +0800

    灵性草-自动化晚安式
    V1.0.0
    1、只上报去掉前导空字符之后以#开头的消息
    2、消息存于云数据库，暂时不支持自定义配置

---
+++ Mack.Club.SkyGrass.AutoGoodNight/MahuaEvents/AutoGoodNightMsg.cs
@@ -0,0 +1,44 @@
+﻿using LxcLibrary.Cache.MySql.lxc_pro_db.auto_good_night_log;
+using LxcLibrary.CommonUtils;
+using Newbe.Mahua;
+using Newbe.Mahua.MahuaEvents;
+using System;
+
+namespace Mack.Club.SkyGrass.AutoGoodNight.MahuaEvents
+{
+    /// <summary>
+    /// 群消息接收事件
+    /// </summary>
+    public class AutoGoodNightMsg
+        : IGroupMessageReceivedMahuaEvent
+    {
+        private readonly IMahuaApi _mahuaApi;
+        private const string dbConnString = "Database=***;Data Source=***;User ID=***;Password=***;CharSet=utf8mb4;SslMode=None;Connect Timeout=60;";
+
+        public AutoGoodNightMsg(
+            IMahuaApi mahuaApi)
+        {
+            _mahuaApi = mahuaApi;
+        }
+
+        public void ProcessGroupMessage(GroupMessageReceivedContext context)
+        {
+            // 只上报#开头的晚安式消息
+            string message = context.Message;
+            message = message.Trim();
+
+            if (message.StartsWith("#"))
+            {
+                ulong from_qq = Convert.ToUInt64(context.FromQq);
+                ulong from_group = Convert.ToUInt64(context.FromGroup);
+                ulong add_time = Convert.ToUInt64(UnixTimestamp.GetTimeStampLong());
+
+                AutoGoodNightLogCache cache = new AutoGoodNightLogCache();
+
+                cache.Init(dbConnString);
+
+                cache.InsertLog(from_qq, from_group, message, add_time);                
+            }
+        }
+    }    
+}
---
+++ Mack.Club.SkyGrass.AutoGoodNight/MySqlCreate/auto_good_night_log.sql
@@ -0,0 +1,18 @@
+﻿# Host: 120.79.45.128  (Version 5.7.20-0ubuntu0.16.04.1)
+# Date: 2019-01-03 22:44:03
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "auto_good_night_log"
+#
+
+CREATE TABLE `auto_good_night_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `from_qq` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `from_group` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `add_time` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `message` varchar(128) NOT NULL DEFAULT '',
+  `status` int(11) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='晚安式消息日志';
